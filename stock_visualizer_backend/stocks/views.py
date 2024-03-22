fbb7545a208e6981f378fef04ba58526f02335a5
briedle <benjaminriedle@gmail.com>
2024-03-22 13:26:27 -0400

    Move some files around and try to get organized

--- stock_visualizer_backend/stocks/views.py
+++ stock_visualizer_backend/stocks/views.py
@@ -1,26 +1,36 @@
+# views.py
+
+import logging
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import BaseStockData  # Assuming you have a model for storing stock data
+from .models import BaseStockData, MonthlyStockPriceData  # Assuming you have a model for storing stock data
+
+
+logger = logging.getLogger(__name__)
 
 def get_symbols(request):
-    symbols = (
-        BaseStockData
-        .objects
-        .values_list('symbol', flat=True)
-        .distinct()
-    )
-    return JsonResponse(list(symbols), safe=False)
+    logger.info("Request hit get_symbols")
+    # symbols = (
+    #     BaseStockData
+    #     .objects
+    #     .values_list('symbol', flat=True)
+    #     .distinct()
+    # )
+    # return JsonResponse(list(symbols), safe=False)
+    return JsonResponse({'status': 'ok'})
 
 def get_time_series(request, symbol):
-    stock_data = (
-        BaseStockData
-        .objects
-        .filter(symbol=symbol)
-        .values(
-            'date', 'open', 'high', 'low', 
-            'close', 'adj_close', 'volume', 'dividend'
-        )
-    )
-    return JsonResponse(list(stock_data), safe=False)
+    logger.info("Request hit get_time_series")
+    # stock_data = (
+    #     MonthlyStockPriceData
+    #     .objects
+    #     .filter(stock__symbol=symbol)
+    #     .values(
+    #         'date', 'open', 'high', 'low', 
+    #         'close', 'adj_close', 'volume', 'dividend'
+    #     )
+    # )
+    # return JsonResponse(list(stock_data), safe=False)
+    return JsonResponse({'status': 'ok'})
 
 
